6415aa27b7d96d1c061c352e0fe4d6e277ca942b
darekarparth <darekarparth18@gmail.com>
2025-09-03 16:35:18 +0100

    Java Tutorial-My first Java Program

---
+++ Java-Tutorials/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+
+}
+// Every line of code that runs in Java must be inside a class. And the class name should always start with an uppercase first letter. In our example, we named the class Main.
+
+// Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+// The name of the java file must match the class name. When saving the file, save it using the class name and add ".java" to the end of the filename. To run the example above on your computer, make sure that Java is properly installed.
+// The main() method is required and you will see it in every Java program:
+// Any code inside the main() method will be executed. Don't worry about the keywords before and after it. You will get to know them bit by bit while reading this tutorial.
+// For now, just remember that every Java program has a class name which must match the filename, and that every program must contain the main() method.
+// Inside the main() method, we can use the println() method to print a line of text to the screen:
